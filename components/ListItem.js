f54056dd47ff6180688c3aecad6f0d309f74381d
Joe Anton <janton153@gmail.com>
2019-10-28 15:26:56 -0700

    Detailed mock data and API getters

--- components/ListItem.js
+++ components/ListItem.js
@@ -19,46 +19,47 @@ import TouchableScale from 'react-native-touchable-scale';
 import { PlayfairText } from '../components/StyledText';
 import { RobotoText } from '../components/StyledText';
 import { withNavigation } from 'react-navigation'
-import {recipes} from '../data/DataArray';
-
+import {recipes, fullRecipes} from '../data/DataArray';
+import {getRecipes} from '../data/MockDataAPI';
 
 class ListItem extends Component {
 
+  onPressRecipe = item => {
+    this.props.navigation.navigate('Details', {item});
+  };
+
+  renderRecipes = ({item}) => (
+    <TouchableScale
+      style={{height: 100, marginBottom: 16, marginLeft:10, marginRight:10, backgroundColor: 'white', flexDirection:'row'}}
+      activeScale={0.95}
+      tension={150}
+      friction={7}
+      useNativeDriver
+      onPress={() => this.onPressRecipe(item)}
+    >
+      <Image
+        source={{uri: item.photo_url}} style={{flex:2,  width: undefined, height: undefined}}
+        resizeMode="cover"/>
+      <View style={{flex:4, justifyContent: 'center'}}>
+        <PlayfairText style={{color:'black', fontSize: 20, paddingBottom:10, paddingLeft: 10, alignSelf:'flex-start'}}>{item.title}</PlayfairText>
+        <RobotoText style={{fontSize: 16, color: 'black', fontWeight:'400', paddingBottom:10, paddingLeft: 10, alignSelf:'flex-start'}} >{item.time} minutes</RobotoText>
+      </View>
+    </TouchableScale>
+  );
+
     render () {
-        //const { data: { title, prep, image}, navigation } = this.props;
+        const recipesArray = getRecipes();
+        console.log(recipesArray);
         return (
           <View style={{flex: 1, paddingTop: 12,}}>
             <FlatList
-            //TODO: fix title/key inconsistency for data type
-            //faltlist requires key to index correctly so changed title to key
               ListHeaderComponent = {
                 <PlayfairText style={{color:'black', fontSize: 46, paddingBottom:16, paddingLeft: 10, alignSelf:'flex-start'}}>Your Recipes</PlayfairText>
               }
 
-              data={ recipes }
-              renderItem={({item, index}) =>
-              //<Text style={styles.item}>{item.key}</Text>
-              <TouchableScale
-                style={{height: 100, marginBottom: 16, marginLeft:10, marginRight:10, backgroundColor: 'white', flexDirection:'row'}}
-                activeScale={0.95}
-                tension={150}
-                friction={7}
-                useNativeDriver
-                onPress={() => this.props.navigation.navigate('Details', {
-                  itemId: 1,
-                  otherParam: "test",
-                  title: item.title,
-                  image: item.image,})}
-              >
-                <Image
-                  source={item.image} style={{flex:2,  width: undefined, height: undefined}}
-                  resizeMode="cover"/>
-                <View style={{flex:4, justifyContent: 'center'}}>
-                  <PlayfairText style={{color:'black', fontSize: 20, paddingBottom:10, paddingLeft: 10, alignSelf:'flex-start'}}>{item.title}</PlayfairText>
-                  <RobotoText style={{fontSize: 16, color: 'black', fontWeight:'400', paddingBottom:10, paddingLeft: 10, alignSelf:'flex-start'}} >{item.prep}</RobotoText>
-                </View>
-              </TouchableScale>
-            }
+              data={ recipesArray }
+              renderItem={this.renderRecipes}
+              keyExractor={item => `${item.recipeId}`}
             />
           </View>
         )
--- data/DataArray.js
+++ data/DataArray.js
@@ -25,3 +25,43 @@ export const recipes = [
       image: require('../assets/images/sesame.jpg')
   }
 ];
+
+export const fullRecipes = [
+  {
+    recipeId: 122,
+    categoryId: 3,
+    title: 'Oatmeal Cookies',
+    photo_url: 'https://www.texanerin.com/content/uploads/2019/06/nobake-chocolate-cookies-1-650x975.jpg',
+    photosArray: [
+      'https://www.texanerin.com/content/uploads/2019/06/nobake-chocolate-cookies-1-650x975.jpg',
+      "https://namelymarly.com/wp-content/uploads/2018/04/20180415_Beet_Lasagna_10.jpg",
+      'https://advancelocal-adapter-image-uploads.s3.amazonaws.com/image.al.com/home/bama-media/width600/img/news_impact/photo/burger-fijpg-57e7e5907630c2ad.jpg',
+      'https://img.thedailybeast.com/image/upload/c_crop,d_placeholder_euli9k,h_1439,w_2560,x_0,y_0/dpr_1.5/c_limit,w_1044/fl_lossy,q_auto/v1492718105/articles/2013/09/24/burger-king-s-new-french-fries-took-ten-years-to-develop/130923-gross-burger-tease_izz59e',
+      'https://aht.seriouseats.com/images/2012/02/20120221-193971-fast-food-fries-Burger-King-fries-2.jpg'
+    ],
+    time: '15',
+    ingredients: [[0, '200ml'], [1, '5g'], [2, '300g']],
+    description:
+      '-- Start with cleaned and peeled russet potatoes that you have cut into 3/8-inch match sticks. Place in bowl of very cold water: keep rinsing and changing the water until the water is clear; drain thoroughly and dry with paper towels or a clean lint-free kitchen towel.\n\n -- Meanwhile, you preheat your hot oil to 350 degrees F. Place prepared taters in oil and cook about 5 minutes. They will have that blond-tone color to them. \n\n -- Note: Once you add cold potatoes to the hot oil, the temperature of your oil is going to drop - you want it to be somewhere between 330 - 325 degrees F. \n\n -- Remove from oil; drain and cool. Now - either refrigerate until ready to finish cooking, or cool completely and freeze up to 3 months. To freeze properly - place completely cooled fries in single layer on tray and place in freezer until frozen. Then bag them.\n\n -- To finish cooking - preheat your oil to 400* F. Add your cold fries (which will drop the oil temp - which is fine because you want it near the 375 degrees F. temp) and cook a few minutes until done. Lightly salt them and shake well so that the salt distributes well and they are not salty.'
+  },
+  {
+    recipeId: 3,
+    categoryId: 4,
+    title: 'Triple Berry Smoothie',
+    photo_url:
+      'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-how-to-make-a-smoothie-horizontal-1542310071.png?crop=0.803xw:0.923xh;0.116xw,0.00510xh&resize=768:*',
+    photosArray: [
+      'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-how-to-make-a-smoothie-horizontal-1542310071.png?crop=0.803xw:0.923xh;0.116xw,0.00510xh&resize=768:*',
+      'https://www.vitamix.com/media/other/images/xVitamix-Triple-Berry-Smoothie-square-crop__1.jpg.pagespeed.ic.OgTC3ILD3R.jpg',
+      'http://images.media-allrecipes.com/userphotos/960x960/3798204.jpg',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrzui8MM6W66I29VZwVvcjpGv99JW3O1owgupc3KwB65rhAyrZ'
+    ],
+    time: '10',
+    ingredients: [
+      [59, '1'],
+      [60, '1/2 lbs'],
+      [61, '1/2 liters'],
+    ],
+    description: 'In a blender, combine all ingredients and blend until smooth. Then divide between 2 cups and top with blackberries, if desired.'
+  },
+];
