d78683b26c5478d7c931993c89303ec744c595ed
Joe Anton <janton153@gmail.com>
2019-10-29 11:11:00 -0700

    Create recipe screen and navigation

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -4,18 +4,20 @@ import {createStackNavigator} from 'react-navigation-stack';
 
 import HomeScreen from '../screens/HomeScreen';
 import DetailScreen from '../screens/DetailScreen';
+import CreateRecipe from '../screens/CreateRecipeScreen';
 import {springyFadeIn} from '../transitions/springyFadeIn';
 const RootStack = createStackNavigator(
   {
     Home: HomeScreen,
     Details: DetailScreen,
+    Create: CreateRecipe,
   },
   {
     initialRouteName: 'Home',
     /* The header config from HomeScreen is now here */
     defaultNavigationOptions: {
       headerStyle: {
-        backgroundColor: '#f4511e',
+        backgroundColor: '#f6b425',
       },
       headerTintColor: '#fff',
       headerTitleStyle: {
--- screens/NewRecipeScreen.js
+++ screens/NewRecipeScreen.js
@@ -1,79 +1,69 @@
 import React, { Component } from 'react';
 import {
-  Text,
   View,
   Image,
   StyleSheet,
-  ScrollView,
-  Dimensions,
   SafeAreaView,
 } from 'react-native';
 import TouchableScale from 'react-native-touchable-scale';
+import { withNavigation } from 'react-navigation'
 import { PlayfairText } from '../components/StyledText';
 import { RobotoText } from '../components/StyledText';
 
-export default class NewRecipe extends Component {
+class NewRecipe extends Component {
 
-render() {
-  return(
-    <SafeAreaView style={styles.container}>
-    <View>
-      <PlayfairText style={styles.titleTextLarge}>Add a new recipe</PlayfairText>
-    </View>
-      <TouchableScale
-        style={styles.card}
-        activeScale={0.95}
-        tension={150}
-        friction={7}
-        useNativeDriver
-        activeOpacity={1}
-        onPress={() =>
-        navigation.navigate('Details', {
-            itemId: 1,
-            otherParam: "test",
-            title: "title",
-            image: "image",
-          })
-        }>
-            <Image
-                source={require('../assets/images/addPhoto.png')} style={{flex:2,  width: undefined, height: undefined}}
-                resizeMode="center"/>
-          <View style={{flex:3, justifyContent: 'center'}}>
-            <PlayfairText style={{color:'black', fontSize: 20, paddingBottom:10, paddingLeft: 10, alignSelf:'flex-start'}}>Basic Photo Recipe</PlayfairText>
-            <RobotoText style={{fontSize: 16, color: 'black', fontWeight:'400', paddingBottom:10, paddingLeft: 10, alignSelf:'flex-start'}} >title(r), photos(r), desc(o), notes(o)</RobotoText>
-          </View>
-      </TouchableScale>
+  onPressCreate = () => {
+    this.props.navigation.navigate('Create');
+  };
 
-      <TouchableScale
-        style={styles.card}
-        activeScale={0.95}
-        tension={150}
-        friction={7}
-        useNativeDriver
-        activeOpacity={1}
-        onPress={() =>
-        navigation.navigate('Details', {
-            itemId: 1,
-            otherParam: "test",
-            title: "title",
-            image: "image",
-          })
-        }>
-            <Image
-                source={require('../assets/images/addRecipe.png')} style={{flex:2,  width: undefined, height: undefined}}
-                resizeMode="center"/>
-          <View style={{flex:3, justifyContent: 'center'}}>
-            <PlayfairText style={{color:'black', fontSize: 20, paddingBottom:10, paddingLeft: 10, alignSelf:'flex-start'}}>Detailed Recipe</PlayfairText>
-            <RobotoText style={{fontSize: 16, color: 'black', fontWeight:'400', paddingBottom:10, paddingLeft: 10, alignSelf:'flex-start'}} >title(r), photo(o),  desc(o), notes(o), ingredients(r), steps(r)</RobotoText>
-          </View>
-      </TouchableScale>
-      <View></View>
-    </SafeAreaView>
-  )
+  render() {
+    return(
+      <SafeAreaView style={styles.container}>
+      <View>
+        <PlayfairText style={styles.titleTextLarge}>Add a new recipe</PlayfairText>
+      </View>
+        <TouchableScale
+          style={styles.card}
+          activeScale={0.95}
+          tension={150}
+          friction={7}
+          useNativeDriver
+          activeOpacity={1}
+          onPress={() => this.onPressCreate()}
+        >
+              <Image
+                  source={require('../assets/images/addPhoto.png')} style={{flex:2,  width: undefined, height: undefined}}
+                  resizeMode="center"/>
+            <View style={{flex:3, justifyContent: 'center'}}>
+              <PlayfairText style={{color:'black', fontSize: 20, paddingBottom:10, paddingLeft: 10, alignSelf:'flex-start'}}>Basic Photo Recipe</PlayfairText>
+              <RobotoText style={{fontSize: 16, color: 'black', fontWeight:'400', paddingBottom:10, paddingLeft: 10, alignSelf:'flex-start'}} >title(r), photos(r), desc(o), notes(o)</RobotoText>
+            </View>
+        </TouchableScale>
+
+        <TouchableScale
+          style={styles.card}
+          activeScale={0.95}
+          tension={150}
+          friction={7}
+          useNativeDriver
+          activeOpacity={1}
+          onPress={() => this.onPressCreate()}
+        >
+              <Image
+                  source={require('../assets/images/addRecipe.png')} style={{flex:2,  width: undefined, height: undefined}}
+                  resizeMode="center"/>
+            <View style={{flex:3, justifyContent: 'center'}}>
+              <PlayfairText style={{color:'black', fontSize: 20, paddingBottom:10, paddingLeft: 10, alignSelf:'flex-start'}}>Detailed Recipe</PlayfairText>
+              <RobotoText style={{fontSize: 16, color: 'black', fontWeight:'400', paddingBottom:10, paddingLeft: 10, alignSelf:'flex-start'}} >title(r), photo(o),  desc(o), notes(o), ingredients(r), steps(r)</RobotoText>
+            </View>
+        </TouchableScale>
+        <View></View>
+      </SafeAreaView>
+    )
+  }
 }
 
-
-}
+export default withNavigation(NewRecipe)
 
 const styles = StyleSheet.create({
   container: {
