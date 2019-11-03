4330a5375a2b65bb8ccde96d6f900c54bb6c1a63
Joe Anton <janton153@gmail.com>
2019-11-03 11:27:56 -0800

    Async firebase data rendering

--- components/ListItem.js
+++ components/ListItem.js
@@ -13,26 +13,43 @@ import {getRecipes} from '../data/MockDataAPI';
 
 //Access Firebase data
 import {db} from '../constants/firebase';
-const itemArr = [];
-
-const itemsRef = db.collection('recipes').get().then(function(querySnapshot) {
-    querySnapshot.forEach(function(doc) {
-        // doc.data() is never undefined for query doc snapshots
-        console.log(doc.id, " => ", doc.data());
-        const data = doc.data();
-        console.log("photo url: " + data.photo_url);
-    });
-})
-.catch(function(error) {
-    console.log("Error getting documents: ", error);
-});
-console.log("itemsRef Object: " + itemsRef);
-//console.log("itemsRef Json: " + toJson(), itemsRef);
-
+//https://levelup.gitconnected.com/react-native-firebase-cloud-firestore-implementing-infinite-scroll-lazy-loading-with-flatlist-a9e942cf66c6
 
 class ListItem extends Component {
 
-  state =
+  state = {
+    itemArr: []
+  }
+
+  componentDidMount = () => {
+  try {
+    // Cloud Firestore: Initial Query
+    this.retrieveData();
+  }
+  catch (error) {
+    console.log(error);
+  }
+};
+
+retrieveData = async () => {
+    console.log("Retrieving Data");
+    try{
+      const initialQuery = await db.collection('recipes');
+      //console.log("Initial Query: " + initialQuery);
+      const docSnapshot = await initialQuery.get();
+      //console.log("Doc snapshot " + docSnapshot);
+      const documentData = docSnapshot.docs.map(document => document.data());
+      //console.log("doc data: " +  documentData);
+      //const lastItem = documentData[0].title;
+      //console.log("last item: " +  lastItem);
+      this.setState({
+        itemArr: documentData
+      })
+    }
+    catch (error) {
+      console.log(error);
+    }
+};
 
   onPressRecipe = item => {
     this.props.navigation.navigate('Details', {item});
@@ -52,43 +69,12 @@ class ListItem extends Component {
         resizeMode="cover"/>
       <View style={{flex:4, justifyContent: 'center'}}>
         <PlayfairText style={{color:'black', fontSize: 20, paddingBottom:10, paddingLeft: 10, alignSelf:'flex-start'}}>{item.title}</PlayfairText>
-        <RobotoText style={{fontSize: 16, color: 'black', fontWeight:'400', paddingBottom:10, paddingLeft: 10, alignSelf:'flex-start'}} >{item.time} minutes</RobotoText>
+        <RobotoText style={{fontSize: 16, color: 'black', fontWeight:'400', paddingBottom:10, paddingLeft: 10, alignSelf:'flex-start'}} >{item.time}</RobotoText>
       </View>
     </TouchableScale>
   );
 
     render () {
-      //const recipesArray = getRecipes();
-
-      // const itemsRef = Object.values(db.collection('recipes').get().then(function(querySnapshot) {
-      //     querySnapshot.forEach(function(doc) {
-      //         // doc.data() is never undefined for query doc snapshots
-      //         //console.log(doc.id, " => ", doc.data());
-      //         //const mydata = doc.data();
-      //         //console.log("photo url: " + doc.data().photo_url);
-      //         //const dataArr = Object.values(mydata);
-      //         //console.log("dataArr " + dataArr);
-      //         //
-      //         itemArr.push({
-      //           recipeId: doc.id,
-      //           title: doc.data().title,
-      //           photo_url: doc.data().photo_url,
-      //           time: doc.data().time,
-      //           ingredients: doc.data().ingredients,
-      //           description: doc.data().description,
-      //           notes: doc.data().notes,
-      //           instructions: doc.data().instructions,
-      //           makes: doc.data().makes,
-      //         });
-      //
-      //         console.log("const: " + itemArr);
-      //     });
-      // })
-      // .catch(function(error) {
-      //     console.log("Error getting documents: ", error);
-      // }));
-
-      console.log("render: " + itemArr);
         return (
           <View style={{flex: 1, paddingTop: 12,}}>
             <FlatList
@@ -96,7 +82,7 @@ class ListItem extends Component {
                 <PlayfairText style={{color:'black', fontSize: 46, paddingBottom:16, paddingLeft: 10, alignSelf:'flex-start'}}>Your Recipes</PlayfairText>
               }
 
-              data={ itemArr }
+              data={ this.state.itemArr }
               renderItem={this.renderRecipes}
             />
           </View>
