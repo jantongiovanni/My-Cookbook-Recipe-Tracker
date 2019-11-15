1c62b3d724974fb565a7cc8f3f6f1f685ff9dda1
Joe Anton <janton153@gmail.com>
2019-11-15 15:54:34 -0800

    Display user information in profile

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,16 +1,63 @@
 import React, {Component} from 'react';
-import { View, Button, Text, ScrollView, StyleSheet, ActivityIndicator } from 'react-native';
+import { View, Button, StyleSheet, Text} from 'react-native';
 import firebase from 'firebase';
+import { RobotoText } from '../components/StyledText';
 
 class ProfileScreen extends Component {
+  constructor(props){
+     super(props);
 
+    this.getUserInfo = this.getUserInfo.bind(this);
+
+    this.state = {
+      name: '',
+      email: '',
+      photoUrl : '',
+      emailVerified : '',
+      uid : ''
+    }
+}
+
+componentDidMount(){
+  this.getUserInfo();
+}
+
+  getUserInfo = () => {
+    var user = firebase.auth().currentUser;
+    if (user != null) {
+      this.setState({
+        name : user.displayName,
+        email : user.email,
+        photoUrl : user.photoURL,
+        emailVerified : user.emailVerified,
+        uid : user.uid
+      });
+
+    } else {
+      console.log("null user");
+    }
+  }
 
   render() {
     return (
       <View style = {styles.container}>
         <Button title="Sign Out"
-          style={{paddingTop: 20}}
           onPress={() => firebase.auth().signOut() }/>
+        <RobotoText style={styles.smallText}>
+          {this.state.name}
+        </RobotoText>
+        <RobotoText style={styles.smallText}>
+          {this.state.email}
+        </RobotoText>
+        <RobotoText style={styles.smallText}>
+          {this.state.photoUrl}
+        </RobotoText>
+        <RobotoText style={styles.smallText}>
+          {this.state.emailVerified ? "true" : "false"}
+        </RobotoText>
+        <RobotoText style={styles.smallText}>
+          {this.state.uid}
+        </RobotoText>
       </View>
     );
   }
@@ -21,6 +68,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
+  smallText : {
+    fontSize: 16,
+    color: 'black',
+    fontWeight:'400',
+    }
 });
