fe3040794345069ab928a788d9f21053d5d2c181
Joe Anton <janton153@gmail.com>
2019-11-14 22:23:51 -0800

    Google web sign in

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,12 +1,33 @@
 import React, {Component} from 'react';
 import { View, Text, ScrollView, StyleSheet, Button } from 'react-native';
+import * as Google from 'expo-google-app-auth';
 
 class LoginScreen extends Component {
+
+  signInWithGoogleAsync = async () => {
+    try {
+      const result = await Google.logInAsync({
+        behavior: 'web',
+        androidClientId: '150393592512-bvm9l2qfa6mkmadldrl0kkoaagpee8dg.apps.googleusercontent.com',
+        //iosClientId: YOUR_CLIENT_ID_HERE,
+        scopes: ['profile', 'email'],
+      });
+
+      if (result.type === 'success') {
+        return result.accessToken;
+      } else {
+        return { cancelled: true };
+      }
+    } catch (e) {
+      return { error: true };
+    }
+  }
+
   render() {
     return (
       <View style = {styles.container}>
         <Button title="Sign In With Google"
-        onPress={() => console.log("button pressed")} />
+          onPress={() => this.signInWithGoogleAsync() }/>
       </View>
     );
   }
