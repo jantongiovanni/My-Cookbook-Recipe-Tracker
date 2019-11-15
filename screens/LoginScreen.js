9e8bae0e187acb2ffd147b4f4aa67cd063a6581f
Joe Anton <janton153@gmail.com>
2019-11-14 22:50:07 -0800

    Sign in and user object handling

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,8 +1,58 @@
 import React, {Component} from 'react';
 import { View, Text, ScrollView, StyleSheet, Button } from 'react-native';
 import * as Google from 'expo-google-app-auth';
+//import {app} from '../constants/firebase';
+import firebase from 'firebase';
 
 class LoginScreen extends Component {
+
+  isUserEqual = (googleUser, firebaseUser) => {
+    if (firebaseUser) {
+      var providerData = firebaseUser.providerData;
+      for (var i = 0; i < providerData.length; i++) {
+        if (providerData[i].providerId === app.auth.GoogleAuthProvider.PROVIDER_ID &&
+            providerData[i].uid === googleUser.getBasicProfile().getId()) {
+          // We don't need to reauth the Firebase connection.
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  onSignIn = (googleUser) => {
+    console.log('Google Auth Response', googleUser);
+    // We need to register an Observer on Firebase Auth to make sure auth is initialized.
+    var unsubscribe = firebase.auth().onAuthStateChanged(function(firebaseUser) {
+      unsubscribe();
+      // Check if we are already signed-in Firebase with the correct user.
+      if (!this.isUserEqual(googleUser, firebaseUser)) {
+        // Build Firebase credential with the Google ID token.
+        var credential = firebase.auth.GoogleAuthProvider.credential(
+            //googleUser.getAuthResponse().id_token);
+            googleUser.idToken,
+            googleUser.accessToken
+          );
+        // Sign in with credential from the Google user.
+        firebase.auth().signInWithCredential(credential).then(function(){
+          console.log("user signed in");
+        })
+        .catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // The email of the user's account used.
+          var email = error.email;
+          // The firebase.auth.AuthCredential type that was used.
+          var credential = error.credential;
+          // ...
+        });
+      } else {
+        console.log('User already signed-in Firebase.');
+      }
+    }.bind(this)
+  );
+  }
 
   signInWithGoogleAsync = async () => {
     try {
@@ -14,6 +64,7 @@ class LoginScreen extends Component {
       });
 
       if (result.type === 'success') {
+        this.onSignIn(result);
         return result.accessToken;
       } else {
         return { cancelled: true };
