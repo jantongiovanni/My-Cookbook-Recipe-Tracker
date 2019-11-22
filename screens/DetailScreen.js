4d701458746b588510f7a368d5b6f1e1e26a9b4d
Joe Anton <janton153@gmail.com>
2019-11-22 13:44:26 -0800

    Switched Saved recipes to use seperate smaller top level collection

--- screens/DetailScreen.js
+++ screens/DetailScreen.js
@@ -1,33 +1,16 @@
 import React from 'react';
 import {
   View,
-  Image,
-  StyleSheet,
-  ScrollView,
-  Dimensions,
-  ActivityIndicator,
-  FlatList,
-  Modal,
-  TouchableOpacity,
-  Alert
 } from 'react-native';
-import TouchableScale from 'react-native-touchable-scale';
-import { PlayfairText } from '../components/StyledText';
-import { RobotoText } from '../components/StyledText';
-import Gallery from 'react-native-image-gallery';
-import {db, storage} from '../constants/firebase';
+
 import firebase from 'firebase';
 
 import DetailRender from '../components/DetailRender';
 import  DetailPlaceholderComponent from '../components/DetailPlaceholder';
 
-const { width: screenWidth } = Dimensions.get('window')
-
 export default class Detail extends React.Component {
 
   state = {
-    count: 0,
-    modalVisible: false,
     user: '',
     savedState: false,
     isDataFetched : false,
@@ -56,20 +39,32 @@ export default class Detail extends React.Component {
     } else {
       console.log("I am a saved recipe");
       console.log("Retrieving Data");
-      // try{
-      //   var user = firebase.auth().currentUser.uid;
-      //   console.log("user: " + user);
-      //   var docRef = item.recipeRef;
-      //   console.log(docSub);
-      //   this.setState({itemArr: await docRef.get(), isDataFetched: true});
-      //
-      //   // await initialQuery.onSnapshot( snapshot => {
-      //   // this.setState({ itemArr : snapshot.docs.map(document => document.data()), isDataFetched: true });
-      //   // });
-      //  }
-      // catch (error) {
-      //   console.log(error);
-      // }
+      try{
+        var user = firebase.auth().currentUser.uid;
+        console.log("user: " + user);
+        var docRef = item.recipeRef;
+        //var docRef = db.collection("recipes").doc("Bow3GEc7quuz6srBkjNr");
+        //console.log(db.docRef.get());
+        await docRef.get().then((doc) => {
+            if (doc.exists) {
+                //console.log("Document data:", doc.data());
+                this.setState({item: doc.data(), isDataFetched: true});
+            } else {
+                // doc.data() will be undefined in this case
+                console.log("No such document!");
+            }
+        }).catch(function(error) {
+            console.log("Error getting document:", error);
+        });
+
+
+        // await initialQuery.onSnapshot( snapshot => {
+        // this.setState({ itemArr : snapshot.docs.map(document => document.data()), isDataFetched: true });
+        // });
+       }
+      catch (error) {
+        console.log(error);
+      }
     }
   }
 
@@ -94,73 +89,3 @@ export default class Detail extends React.Component {
     )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  saveButtonText: {
-    color: '#FFFFFF',
-    fontSize: 20,
-    textAlign: 'center',
-    fontWeight: 'normal',
-  },
-  saveButton: {
-    borderWidth: 1,
-    borderColor: '#f6b425',
-    backgroundColor: '#f6b425',
-    padding: 15,
-    marginHorizontal: 20,
-    marginBottom: 20,
-  },
-  topImage:{
-    flex:1,
-    width: screenWidth,
-    height: screenWidth,
-    alignSelf: 'flex-start'
-  },
-
-  titleTextLarge:{
-    fontSize: 50,
-    color: 'black',
-    paddingLeft: 20,
-    paddingTop: 14,
-  },
-  subtitleText:{
-    fontSize: 36,
-    color: 'black',
-    paddingBottom: 14,
-    paddingLeft: 20,
-  },
-  titleTextMin:{
-    fontSize: 24,
-    color: 'black',
-    paddingLeft: 20,
-    fontWeight: '100',
-  },
-  numberText:{
-    fontSize: 36,
-    color: '#f6b425',
-    paddingTop: 16,
-    paddingLeft: 20,
-
-  },
-  contentText:{
-    fontSize: 20,
-    color: 'black',
-    fontWeight:'400',
-    paddingTop: 20,
-    paddingLeft: 20,
-    marginRight:20,
-    paddingRight: 20,
-  },
-  line:{
-    borderBottomColor: '#D3D3D3',
-    borderBottomWidth: 1,
-    paddingTop: 36,
-    marginBottom: 30,
-    marginLeft: 20,
-    borderTopLeftRadius: 10,
-    borderBottomLeftRadius: 10,
-  },
-});
