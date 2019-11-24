b2a18c0d7dfe3748622fa7d6d90a08ae357a25b0
Joe Anton <janton153@gmail.com>
2019-11-24 11:28:39 -0800

    Added tab view to profile screen

--- components/ProfileContainer.js
+++ components/ProfileContainer.js
@@ -2,23 +2,53 @@ import React, { Component } from 'react';
 import {
   View,
   ScrollView,
-  SafeAreaView
+  SafeAreaView,
+  Dimensions
   } from 'react-native';
-  import { withNavigation } from 'react-navigation'
+import { withNavigation } from 'react-navigation'
+import { TabView, SceneMap } from 'react-native-tab-view';
 
 import ListItem from '../components/ListItem';
 import ProfileScreen from '../screens/ProfileScreen';
+import SavedScreen from '../screens/SavedScreen'
+
+const FirstRoute = () => (
+  <ListItem/>
+);
+
+const SecondRoute = () => (
+  <SavedScreen/>
+);
+
 
 class HomeContainer extends Component {
   static navigationOptions = ({ navigation }) => ({
     header: null,
   });
+
+  state = {
+      index: 0,
+      routes: [
+        { key: 'first', title: 'First' },
+        { key: 'second', title: 'Second' },
+      ],
+    };
+
+
   render () {
       return (
         <SafeAreaView style={{flex: 1}}>
           <ScrollView style={{flex: 1}}>
             <ProfileScreen/>
-            <ListItem/>
+            <TabView
+              navigationState={this.state}
+              renderScene={SceneMap({
+                first: FirstRoute,
+                second: SecondRoute,
+              })}
+              onIndexChange={index => this.setState({ index })}
+              initialLayout={{ width: Dimensions.get('window').width }}
+            />
           </ScrollView>
         </SafeAreaView>
       )};
