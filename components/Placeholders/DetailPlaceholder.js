8fad9403331e9dbf28eb1bf52946c938cf815990
jantongiovanni <janton153@gmail.com>
2019-11-27 16:26:09 -0800

    Detail Placeholder matches more accurately

--- components/Placeholders/DetailPlaceholder.js
+++ components/Placeholders/DetailPlaceholder.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Dimensions } from 'react-native';
 import {
   Placeholder,
   PlaceholderMedia,
@@ -8,28 +8,32 @@ import {
 } from 'rn-placeholder';
 import { PlayfairText } from '../../components/StyledText';
 
+const { width: screenWidth } = Dimensions.get('window')
+
 const DetailPlaceholderComponent = () => (
   <View>
-    <PlayfairText style={{color:'black', fontSize: 46, paddingBottom:16, paddingLeft: 10, alignSelf:'flex-start'}}>Detail Placeholder</PlayfairText>
+    <View
+      style={{
+          height: screenWidth,
+          width: screenWidth,
+          backgroundColor: "white"
+      }}>
+    </View>
 
     <Placeholder
+      style={{margin:20}}
+      Animation={ShineOverlay}
+    >
+      <PlaceholderLine width={screenWidth-20} height={40} />
+    </Placeholder>
+    <Placeholder
       style={{height: 100, margin:20, backgroundColor: 'white'}}
-      Left={PlaceholderMedia}
       Animation={ShineOverlay}>
       <PlaceholderLine width={80} />
       <PlaceholderLine width={30} />
     </Placeholder>
     <Placeholder
       style={{height: 100, margin:20, backgroundColor: 'white'}}
-      Left={PlaceholderMedia}
-      Animation={ShineOverlay}>
-      <PlaceholderLine width={80} />
-      <PlaceholderLine />
-      <PlaceholderLine width={30} />
-    </Placeholder>
-    <Placeholder
-      style={{height: 100, margin:20, backgroundColor: 'white'}}
-      Left={PlaceholderMedia}
       Animation={ShineOverlay}>
       <PlaceholderLine width={80} />
       <PlaceholderLine />
@@ -37,7 +41,6 @@ const DetailPlaceholderComponent = () => (
     </Placeholder>
     <Placeholder
       style={{height: 100, margin:20, backgroundColor: 'white'}}
-      Left={PlaceholderMedia}
       Animation={ShineOverlay}>
       <PlaceholderLine width={80} />
       <PlaceholderLine />
@@ -45,7 +48,6 @@ const DetailPlaceholderComponent = () => (
     </Placeholder>
     <Placeholder
       style={{height: 100, margin:20, backgroundColor: 'white'}}
-      Left={PlaceholderMedia}
       Animation={ShineOverlay}>
       <PlaceholderLine width={80} />
       <PlaceholderLine />
