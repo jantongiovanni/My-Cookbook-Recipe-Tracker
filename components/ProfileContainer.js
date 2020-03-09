caf4131978560d74f2c912d5e2fe5ff7547b01f3
Joe Anton <janton153@gmail.com>
2020-03-08 17:04:36 -0700

    Restyled tab bar

--- components/ProfileContainer.js
+++ components/ProfileContainer.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
 import {
-  View,
   ScrollView,
   SafeAreaView,
   Dimensions,
   StyleSheet,
-  TouchableOpacity
   } from 'react-native';
 import { withNavigation } from 'react-navigation'
-import { TabView, SceneMap } from 'react-native-tab-view';
-import Animated from 'react-native-reanimated';
-
+import { TabView, TabBar, SceneMap } from 'react-native-tab-view';
 import ListItem from '../components/ListItem';
 import ProfileScreen from '../screens/ProfileScreen';
 import SavedScreen from '../screens/SavedScreen'
@@ -22,7 +18,6 @@ const FirstRoute = () => (
 const SecondRoute = () => (
   <SavedScreen/>
 );
-
 
 class HomeContainer extends Component {
   static navigationOptions = ({ navigation }) => ({
@@ -37,57 +32,35 @@ class HomeContainer extends Component {
       ],
     };
 
-  _renderTabBar = props => {
-  const inputRange = props.navigationState.routes.map((x, i) => i);
-
+renderTabBar(props) {
   return (
-    <View style={styles.tabBar}>
-      {props.navigationState.routes.map((route, i) => {
-        const color = Animated.color(
-          Animated.round(
-            Animated.interpolate(props.position, {
-              inputRange,
-              outputRange: inputRange.map(inputIndex =>
-                inputIndex === i ? 255 : 0
-              ),
-            })
-          ),
-          0,
-          0
-        );
-
-        return (
-          <TouchableOpacity
-            key={i}
-            style={styles.tabItem}
-            onPress={() => this.setState({ index: i })}>
-            <Animated.Text style={{ color }}>{route.title}</Animated.Text>
-          </TouchableOpacity>
-        );
-      })}
-    </View>
+  <TabBar
+    style={{backgroundColor: '#f7f7f7', elevation: 10, borderColor: '#000000', borderBottomWidth: 0, height:50}}
+    labelStyle={{color: 'black', fontSize: 14, fontWeight: 'normal'}}
+    {...props}
+    indicatorStyle={{backgroundColor: 'black', height: 2}}
+  />
   );
-};
+}
 
-
-  render () {
-      return (
-        <SafeAreaView style={{flex: 1}}>
-          <ScrollView style={{flex: 1}}>
-            <ProfileScreen/>
-            <TabView
-              navigationState={this.state}
-              renderTabBar={this._renderTabBar}
-              renderScene={SceneMap({
-                first: FirstRoute,
-                second: SecondRoute,
-              })}
-              onIndexChange={index => this.setState({ index })}
-              initialLayout={{ width: Dimensions.get('window').width }}
-            />
-          </ScrollView>
-        </SafeAreaView>
-      )};
+render () {
+    return (
+      <SafeAreaView style={{flex: 1}}>
+        <ScrollView style={{flex: 1}}>
+          <ProfileScreen/>
+          <TabView
+            navigationState={this.state}
+            renderTabBar={this.renderTabBar}
+            renderScene={SceneMap({
+              first: FirstRoute,
+              second: SecondRoute,
+            })}
+            onIndexChange={index => this.setState({ index })}
+            initialLayout={{ width: Dimensions.get('window').width }}
+          />
+        </ScrollView>
+      </SafeAreaView>
+    )};
 }
 export default withNavigation(HomeContainer);
 
@@ -95,17 +68,5 @@ export default withNavigation(HomeContainer);
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  tabBar: {
-    flexDirection: 'row',
-    marginTop: 20,
-    borderRadius: 4,
-    borderWidth: 0.5,
-    borderColor: 'grey',
-  },
-  tabItem: {
-    flex: 1,
-    alignItems: 'center',
-    padding: 16,
-  },
+  }
 });
